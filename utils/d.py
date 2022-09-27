fb10c11b91556f0beb505d4d2e854fc9b062dd43
Young <154995494@qq.com>
2022-09-28 00:19:46 +0800

    update

--- utils/d.py
+++ utils/d.py
@@ -5,42 +5,60 @@ from eth_account import Account
 import codecs
 import os
 
-digits = 32
+import warnings
+warnings.filterwarnings("ignore")
 
-txtName = "text.txt"
-f = open(txtName, "w")
-for i in range(1, 1000):
+account_info = "account_info.txt"  # 存放地址和秘钥
+addr = open(account_info, "w")
+
+address_a = "address.txt"  # 存放地址
+addre = open(address_a, "w")
+
+i = 0
+while True:
+    # 生成密钥
+    digits = 32
     hex = codecs.encode(os.urandom(digits), 'hex').decode()
-    new_context = hex + '\n'
-    f.write(new_context)
+    sleep(0.8)
 
-f.close()
+    # 处理密钥，密钥生成地址切换使用.address方法
+    acct = Account.from_key(hex)
 
-sleep(1)
-all_the_text = open('text.txt').read()
-new3_list = all_the_text.split("\n")
+    # 发起请求，查找地址余额
+    u = "https://api.etherscan.io/api"
+    data = {"module": "account",
+            "action": "balance",
+            "address": acct.address,
+            "tag": "latest",
+            "apikey": "NV5EPER64FIAXQRD89IJ3Y1C5GP942ZUR9"}
+    try:
+        res = requests.post(u, params=data, verify=False)
+        # print(res.json())
 
-acount_info = "acount_info.txt"  # 存放地址和秘钥
-addr = open(acount_info, "w")
+        if i % 100 == 0:
+            print(res.json(), "执行第{}次".format(i))
 
-address = "address.txt"  # 存放地址
-addre = open(address, "w")
-for i in new3_list[:-1]:
-    acct = Account.from_key(i)
-    acount_info = str(acct.address) + ", " + str(i) + "\n"
-    address_info = acct.address + "\n"
-    addr.write(acount_info)  # 写入地址和秘钥
-    addre.write(address_info)  # 写入地址
+        # 如果地址余额大于0则保存密钥和地址
+        if int(res.json()["result"]) > 0:
+            account_info = str(acct.address) + ", " + str(hex) + "\n"
+            print(res.json())
+            print(account_info)
+            addr.write(account_info)  # 写入有效地址和秘钥
+
+        # 保存所有密钥和地址
+        address_s = str(acct.address) + ", " + str(hex) + "\n"
+        addre.write(address_s)  # 写入地址和密钥
+
+        # 执行次数
+        i += 1
+
+        if res.json()["status"] != "1":
+            break
+
+    except requests.exceptions.SSLError:
+        pass
 
 addr.close()
 addre.close()
 
-u = "https://api.etherscan.io/api"
-data = {
-    "module": "account",
-    "action": "tokenbalance",
-    "contractaddress": "0x47e5188894284c8aaD9e50AcF67E6DCa13cd7AeB",
-    "address": "0x47e5188894284c8aaD9e50AcF67E6DCa13cd7AeB",
-    "tag": "latest",
-    "apikey": "NV5EPER64FIAXQRD89IJ3Y1C5GP942ZUR9"
-}
+print(i)
